9083a4b31205585ae4cd862d434e6095a1b7048e
Vladislav Botvin <Vladislav.Botvin@infowatch.com>
2015-07-12 01:27:03 +0300

    remove excess in jx.sh

--- jx.sh
+++ jx.sh
@@ -4,7 +4,7 @@ regenerator --include-runtime gulpfile.js gulpfile.js
 regenerator --include-runtime ./core ./core
 replace \.coffee \.js ./core/*.js -r
 replace \.coffee \.js gulpfile.js
-jx package gulpfile.js tavmant.jx -add "core/*.js" -slim @dev,@prod,assets,layouts,node_modules,pages,partials,settings,site_root,gulpfile.coffee,index.js,jx.sh,package.json
+jx package gulpfile.js tavmant.jx -add "core/*.js"
 rm gulpfile.js
 rm *.jxp
 rm core/*.js
@@ -12,4 +12,3 @@ rm core/.module-cache/*.js
 rm core/.module-cache/manifest/*.json
 zip tavmant-$(git describe).zip package.json server.sh build.sh reinstall.sh tavmant.jx
 rm tavmant.jx
-
