931d109948f97d3d982654463e3189eedc0e94b9
Vladislav Botvin <Vladislav.Botvin@infowatch.com>
2015-07-12 22:41:17 +0300

    minify js in jx

--- jx.sh
+++ jx.sh
@@ -3,6 +3,7 @@ replace \.coffee \.js ./core/*.js -r
 replace \.coffee \.js gulpfile.js
 browserify --entry gulpfile.js --exclude "key.js" --exclude "node_modules/**/*.*" --node --outfile tavmant_temp.js
 regenerator --include-runtime tavmant_temp.js > tavmant.js
+uglifyjs tavmant.js -c -m -o tavmant.js
 jx package tavmant.js tavmant.jx -add tavmant.js
 rm gulpfile.js core/*.js tavmant.js tavmant_temp.js *.jxp
 zip tavmant-$(git describe).zip package.json server.sh build.sh reinstall.sh tavmant.jx
