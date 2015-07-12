7a1c6a8940383a4430c0951b0b7f7ce6dab00dd1
Vladislav Botvin <Vladislav.Botvin@infowatch.com>
2015-07-12 23:14:54 +0300

    several steps to build jx

--- jx1.sh
+++ jx1.sh
@@ -4,7 +4,3 @@ replace \.coffee \.js gulpfile.js
 browserify --entry gulpfile.js --exclude "key.js" --exclude "node_modules/**/*.*" --node --outfile tavmant_temp.js
 regenerator --include-runtime tavmant_temp.js > tavmant.js
 uglifyjs tavmant.js -c -m -o tavmant.js
-jx package tavmant.js tavmant.jx -add tavmant.js
-rm gulpfile.js core/*.js tavmant.js tavmant_temp.js *.jxp
-zip tavmant-$(git describe).zip package.json server.sh build.sh reinstall.sh tavmant.jx
-rm tavmant.jx
