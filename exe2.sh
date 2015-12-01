2428c1f5db8ac149ef79f978b0e1568659083a96
Vladislav Botvin <darkvlados@me.com>
2015-12-01 19:01:53 +0300

    remove reinstall.sh

--- exe2.sh
+++ exe2.sh
@@ -1,2 +1,2 @@
-zip tavmant-$(git describe).zip server.sh build.sh reinstall.sh tavmant.js version.txt
+zip tavmant-$(git describe).zip server.sh build.sh tavmant.js version.txt
 rm gulpfile.js core/*.js tavmant.js
