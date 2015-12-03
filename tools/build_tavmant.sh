73850073afc7b7d61917382a6c9f55f906f7affc
Vladislav Botvin <darkvlados@me.com>
2015-12-03 12:47:10 +0300

    fix build_tavmant

--- tools/build_tavmant.sh
+++ tools/build_tavmant.sh
@@ -3,4 +3,5 @@ replace \.coffee \.js ./core/*.js -r
 replace \.coffee \.js gulpfile.js
 lsc tools/concat_tavmant.coffee
 uglifyjs --output tavmant.js tavmant.js
+lsc tools/encrypt_tavmant.coffee
 rm gulpfile.js core/*.js tavmant.js
