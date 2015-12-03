1652b15ebbdabad102b2043a0187dfc257c110af
Vladislav Botvin <darkvlados@me.com>
2015-12-03 04:44:30 +0300

    remove get_license_key tool

--- tools/get_license_key.js
+++
@@ -1,9 +0,0 @@
-var sha1 = require("crypto").createHash("sha1"),
-  decipher = require("crypto").createDecipher("aes-256-cbc", "1WRhJ4ApQLDQu6GxZOHiPYPCZbsn9Jvq");
-
-console.log(
-  sha1.update(
-    `${decipher.update(process.argv[2], "hex", "utf8")}${decipher.final("utf8")}`,
-    "utf8"
-  ).digest("hex")
-);
