e4a9b1af429d6e416d7e7480907b2220c68ce929
Vladislav Botvin <darkvlados@me.com>
2015-12-07 10:15:19 +0300

    mount project tool

---
+++ tools/mount_project.sh
@@ -0,0 +1,9 @@
+rm assets categories javascript layouts pages partials settings site_root templates
+ln -s ../$1/assets assets
+ln -s ../$1/categories categories
+ln -s ../$1/layouts layouts
+ln -s ../$1/pages pages
+ln -s ../$1/partials partials
+ln -s ../$1/settings settings
+ln -s ../$1/site_root site_root
+ln -s ../$1/templates templates
