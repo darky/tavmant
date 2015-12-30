27a9c33a0a5c9d7d758dab41e806adea8181c72b
Vladislav Botvin <darkvlados@me.com>
2015-12-31 02:10:40 +0300

    exclude livescript from browserify

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-require("livescript");
-if (!require.extensions) {
-  require.extensions = {};
-}
-require.extensions[".coffee"] = require.extensions[".ls"];
 if (process.env.TAVMANT_DEV) {
+  global.require("livescript");
+  global.require.extensions[".coffee"] = global.require.extensions[".ls"];
   global.require("remote").getCurrentWebContents().openDevTools();
 }
 require("./gulp/gulpfile.coffee");
