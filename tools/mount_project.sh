b3fe88ea0a52a26c7ae4d0477dbd5f4fc8cfcd15
Vladislav Botvin <darkvlados@me.com>
2016-01-02 22:58:14 +0300

    categories -> db

--- tools/mount_project.sh
+++ tools/mount_project.sh
@@ -1,7 +1,7 @@
-rm api assets categories layouts pages partials settings site_root templates
+rm api assets db layouts pages partials settings site_root templates
 ln -s ../$1/api api
 ln -s ../$1/assets assets
-ln -s ../$1/categories categories
+ln -s ../$1/db db
 ln -s ../$1/layouts layouts
 ln -s ../$1/pages pages
 ln -s ../$1/partials partials
