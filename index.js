79dc124a0c0afa9e5f965a0bf3778d1c731e4fb6
Vladislav Botvin <darkvlados@me.com>
2016-01-10 19:45:01 +0300

    correct dev tools extension path

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 if (process.env.TAVMANT_DEV) {
   global.require("livescript");
-  global.require("remote").BrowserWindow.addDevToolsExtension("/Users/darky/Library/Application Support/Google/Chrome/Default/Extensions/bomhdjeadceaggdgfoefmpeafkjhegbo/2.0.5_0");
+  global.require("remote").BrowserWindow.addDevToolsExtension(process.env.HOME + "/Library/Application Support/Google/Chrome/Default/Extensions/bomhdjeadceaggdgfoefmpeafkjhegbo/2.0.5_0");
   global.require("remote").getCurrentWebContents().openDevTools();
 }
 require("./gulp/gulpfile.ls");
