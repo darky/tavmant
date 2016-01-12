ff6ed674053e1d96ea6d8d1304806281c9c5e0f0
Vladislav Botvin <darkvlados@me.com>
2016-01-12 19:13:03 +0300

    correct build_tavmant.sh

--- tools/build_tavmant.sh
+++ tools/build_tavmant.sh
@@ -1,10 +1,10 @@
 lsc -c ./electron_frontend/routes.ls ./electron_frontend/**/*.ls ./gulp/*.ls common.ls
-replace \.ls \.js ./gulp/*.js -r
-replace \.ls \.js ./electron_frontend/**/*.js -r
-replace \.ls \.js ./electron_frontend/routes.js -r
-replace \.ls \.js common.js
+replace "\.ls\"" ".js\"" ./gulp/*.js -r
+replace "\.ls\"" ".js\"" ./electron_frontend/**/*.js -r
+replace "\.ls\"" ".js\"" ./electron_frontend/routes.js -r
+replace "\.ls\"" ".js\"" common.js
 cp index.js tavmant.js
-replace \.ls \.js tavmant.js
+replace "\.ls\"" ".js\"" tavmant.js
 lsc tools/concat_tavmant.ls
 uglifyjs --output tavmant2.js tavmant2.js
 lsc tools/encrypt_tavmant.ls
