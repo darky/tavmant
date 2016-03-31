1103ba202c3528c914fd6e13676605e2d6cc0de5
Vladislav Botvin <dark-vlados@mail.ru>
2016-03-31 23:24:55 +0300

    remove hardcode black theme

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-if (process.env.TAVMANT_DEV) {
-  global.require("livescript");
-  global.require("remote").BrowserWindow.addDevToolsExtension(process.env.HOME + "/.config/chromium/Default/Extensions/bomhdjeadceaggdgfoefmpeafkjhegbo/2.0.7_0");
-  global.require("remote").getCurrentWebContents().openDevTools();
-}
+require("livescript");
+require("remote").getCurrentWebContents().openDevTools();
 require("./gulp/gulpfile.ls");
 require("./electron_frontend/routes.ls");
